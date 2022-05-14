cc5d70f39a32cc87968d78a4c31534b053a41c0b
exrol <exrol@protonmail.com>
2022-05-14 13:56:27 +0200

    implement first basic version

--- example/src/App.js
+++ example/src/App.js
@@ -4,17 +4,21 @@ import useChessClock from 'react-chess-clock'
 
 const App = () => {
 
-  const [whiteTimer, blackTimer, toggleTimer, pauseTimer, resetTimer] = useChessClock()
-     
+  const [whiteTimer, blackTimer, startTimer, activePlayer, toggleTimer, togglePause, resetTimer] = useChessClock()
+
   //Basic display
   return (
-      <div>
-          <p>White timer : {whiteTimer}</p>
-          <p>Black timer : {whiteTimer}</p>
-          <button onClick={() => toggleTimer()}>Toggle</button>
-          <button onClick={() => resetTimer()}>Reset Timer</button>       
-          <button onClick={() => pauseTimer()}>Pause Timer</button>          
-      </div>
+    <div>
+
+      <p>Active Player : {activePlayer}</p>
+      <br />
+      <p>White timer : {whiteTimer}</p>
+      <p>Black timer : {blackTimer}</p>
+      <button onClick={() => toggleTimer()}>Toggle</button>
+      <button onClick={() => resetTimer()}>Reset Timer</button>
+      <button onClick={() => togglePause()}>Pause Timer</button>
+      <button onClick={() => startTimer()}>Start</button>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,60 @@
-import React, { useState }  from 'react'
+import React, { useState, useEffect } from 'react'
 
 // React chess clock hook
-export default function useChessClock (initialTimer = 600, initialIncrement = 0){
-  
-    // Clock state
-    const [timer, setTimer] = useState(initialTimer);
-    const [increment, setIncrement] = useState(initialIncrement);
+export default function useChessClock(initialTimer = 3, increment = 5) {
 
-    //Player timers
-    const [whiteTimer, setwhiteTimer] = useState(initialTimer);
-    const [blackTimer, setBlackTimer] = useState(initialTimer);
+  //Player timers
+  const [whiteTimer, setWhiteTimer] = useState(initialTimer * 1000); // set time in milliseconds
+  const [blackTimer, setBlackTimer] = useState(initialTimer * 1000);
 
-    //Utils functions
-    const toggleTimer = () => console.log('toggle')
-    const pauseTimer = () => console.log('toggle')
-    const resetTimer = () => console.log('reset')
-    
-    return [whiteTimer, blackTimer, toggleTimer, pauseTimer, resetTimer]
+  //Clock logic
+  const [activePlayer, setActivePlayer] = useState(null);
+  const [pausedPlayer, setPausedPlayer] = useState(null)
+
+  const isClockActive = activePlayer !== null;
+  const setActiveTimer = activePlayer === 'white' ? setWhiteTimer : setBlackTimer
+  const activeTimer = activePlayer === 'white' ? whiteTimer : blackTimer
+
+  const isGameOver = blackTimer <= 0 || whiteTimer <= 0
+  useEffect(() => {
+    let interval;
+
+    if (isClockActive && !isGameOver) {
+      interval = setInterval(() => {
+        setActiveTimer((timer) => timer - 10);
+      }, 10);
+    }
+
+    return () => clearInterval(interval);
+  }, [activePlayer]);
+
+  const startTimer = () => {
+
+    if (activePlayer !== null) {
+      return;
+    }
+
+    setActivePlayer('white');
+  }
+
+  const toggleTimer = () => activePlayer === 'white' ? setActivePlayer('black') : setActivePlayer('white')
+
+  const togglePause = () => {
+    if (pausedPlayer) {
+      setActivePlayer(pausedPlayer);
+      setPausedPlayer(null);
+    } else {
+      setPausedPlayer(activePlayer);
+      setActivePlayer(null);
+    }
+  }
+
+  const resetTimer = () => {
+    setActivePlayer(null)
+    setBlackTimer(initialTimer * 1000);
+    setWhiteTimer(initialTimer * 1000);
+  }
+
+  return [whiteTimer, blackTimer, startTimer, activePlayer, toggleTimer, togglePause, resetTimer]
 
 }
